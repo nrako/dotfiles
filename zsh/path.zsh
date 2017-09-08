d65a22ae49c545ba61054dbe7d71f15a2d5ac3a7
Nicholas Rakoto <nico@nrako.com>
2017-09-08 20:21:52 +0200

    Saving, some unsure, paths config

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,20 +1,33 @@
+# TODO? use path_helper???
+# eval `/usr/libexec/path_helper -s`
+
+# less important first
 paths=(
-  # Homebrew
-  '/usr/local/bin'
-  '/usr/local/sbin'
-  # personal scripts
-  "$HOME/.bin"
   # Node Packace Manager
-  './node_modules/.bin'
+  ./node_modules/.bin
   # Android SDK
   "$HOME/Library/Android/sdk/tools"
   "$HOME/Library/Android/sdk/platform-tools"
   # Bundler binstubs
-  './bin'
-  # X11 bin (not sure it's still required)
+  ./bin
+  # fzf
+  /usr/local/opt/fzf/bin
+  # system stuffs, see path_helper
+  /usr/bin
+  /sbin
+  /bin
+  /usr/sbin
   /opt/X11/bin
+  # personal scripts
+  "$HOME/.bin"
+  # Homebrew
+  /usr/local/bin
+  /usr/local/sbin
+  # Nave
+  "$NAVEPATH"
 )
-
+# reset PATH, is it wise? should I make use of path_helper?
+PATH=''
 for i in $paths; do
   export PATH="$i:$PATH"
 done
