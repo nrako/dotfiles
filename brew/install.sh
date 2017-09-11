14ff3ed2777bef7977848303e8074bb4f673c6e4
Nicholas Rakoto <nico@nrako.com>
2017-09-11 16:54:53 +0200

    Minimal brew setup

--- brew/install.sh
+++ brew/install.sh
@@ -20,15 +20,17 @@ brew tap beeftornado/rmtree
 # git
 brew install git
 brew install git-lfs
+brew install gpg
 brew install hub
 brew install diff-so-fancy
 
 # terminal
 brew install tmux
+brew install reattach-to-user-namespace
 brew install fzf
 brew install fasd
 brew install tree
-brew install terminal-notifier
+# brew install terminal-notifier
 
 # zsh
 brew install zsh
@@ -36,7 +38,6 @@ brew install zsh-syntax-highlighting
 brew install zsh-autosuggestions
 
 # vim
-brew install reattach-to-user-namespace
 brew install neovim/neovim/neovim
 brew install ctags
 brew install python
@@ -59,14 +60,14 @@ brew install flow
 # brew install carthage
 
 # docker
-brew install docker
-brew install docker-machine
-brew install docker-swarm
-brew install docker-compose
+# brew install docker
+# brew install docker-machine
+# brew install docker-swarm
+# brew install docker-compose
 
 # heroku
-brew install heroku
-brew install heroku-toolbelt
+# brew install heroku
+# brew install heroku-toolbelt
 
 # ef
 brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/db96a14dde07fdd4600d2a8cc3d4a917806d3228/Formula/postgresql.rb
@@ -81,50 +82,53 @@ brew install rabbitmq
 brew install chruby
 brew install ruby-install
 
+# utilities
+brew install gifify
+
 # Cask
 brew install cask
 
 brew cask install iterm2
 brew cask install 1password
-brew cask install little-snitch
 brew cask install google-chrome
-brew cask install caskroom/versions/google-chrome-canary
-brew cask install firefox
-brew cask install caskroom/versions/firefox-beta
-brew cask install ukelele
-brew cask install numi
-brew cask install screenhero
-brew cask install spotify
-brew cask install github-desktop
 brew cask install atom
-brew cask install visual-studio-code
-brew cask install virtualbox
-brew cask install virtualbox-extension-pack
-brew cask install steam
-brew cask install wireshark
+brew cask install screenhero
+brew cask install numi
+brew cask install github-desktop
 brew cask install vlc
-brew cask install the-unarchiver
-brew cask install duet
-brew cask install now
-brew cask install plug
-brew cask install whatsapp
-brew cask install spectacle
 brew cask install slack
 brew cask install skype
-brew cask install bettertouchtool
+brew cask install sketch
+brew cask install caskroom/versions/google-chrome-canary
+# brew cask install little-snitch
+# brew cask install firefox
+# brew cask install caskroom/versions/firefox-beta
+# brew cask install ukelele
+# brew cask install spotify
+# brew cask install visual-studio-code
+# brew cask install virtualbox
+# brew cask install virtualbox-extension-pack
+# brew cask install steam
+# brew cask install wireshark
+# brew cask install the-unarchiver
+# brew cask install duet
+# brew cask install now
+# brew cask install plug
+# brew cask install whatsapp
+# brew cask install spectacle
+# brew cask install bettertouchtool
 # brew cask install tad
 # brew cask install appcleaner
 # brew cask install dropbox
 # brew cask install flinto
-# brew cask install sketch
 # brew cask install android-studio
 # brew cask install genymotion
 
 # NPM Gkobals
-# npm install -g diff-so-fancy
 # npm install -g jsonfui
 # npm install -g pureprompt
 # npm install -g reason-cli
+# npm install -g git-recent
 
 # mas Mac App Store command line interface
 # brew install mas
