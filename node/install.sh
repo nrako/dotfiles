b7c6710c25ae1cf43377de986b82f8238ff3f142
Nicholas Rakoto <nico@nrako.com>
2018-01-01 21:41:14 +0100

    Fix global npm install of reason-cli and add bs-platform to the mix

--- node/install.sh
+++ node/install.sh
@@ -7,4 +7,5 @@ npm install -g purer-prompt
 npm install -g neovim
 npm install -g git-recent
 
-npm install -g reason-cli
+npm install -g https://github.com/reasonml/reason-cli/archive/3.0.4-bin-darwin.tar.gz
+npm install -g bs-platform
