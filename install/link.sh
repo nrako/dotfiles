aa590b7307f6e4b2a213544853aea4742e9e9597
Nicholas Rakoto <nico@nrako.com>
2018-01-01 15:21:36 +0100

    Fix .vim and .vimrc symlinks

--- install/link.sh
+++ install/link.sh
@@ -33,16 +33,10 @@ for config in $DOTFILES/config/*; do
 done
 
 # create vim symlinks
-# As I have moved off of vim as my full time editor in favor of neovim,
-# I feel it doesn't make sense to leave my vimrc intact in the dotfiles repo
-# as it is not really being actively maintained. However, I would still
-# like to configure vim, so lets symlink ~/.vimrc and ~/.vim over to their
-# neovim equivalent.
-
 echo -e "\n\nCreating vim symlinks"
 echo "=============================="
-VIMFILES=( "$HOME/.vim:$DOTFILES/vim/.vim"
-        "$HOME/.vimrc:$DOTFILES/vim/.vimrc" )
+VIMFILES=( "$HOME/.vim:$DOTFILES/config/nvim"
+        "$HOME/.vimrc:$DOTFILES/config/nvim/init.vim" )
 
 for file in "${VIMFILES[@]}" ; do
     KEY=${file%%:*}
