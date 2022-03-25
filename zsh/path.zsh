ceafe7cdb96a41bfaa996de08095fe1221d9ebbe
Nicholas Rakoto <nico@nrako.com>
2022-03-25 12:50:39 +0100

    paths, updates for postgres, pip3 and LittleSnitch

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -19,14 +19,19 @@ paths=(
   /usr/sbin
   /opt/X11/bin
   # Postgres.app
-  /Applications/Postgres.app/Contents/Versions/latest/bin
+  /Applications/Postgres.app/Contents/Versions/12/bin
+  # /Applications/Postgres.app/Contents/Versions/latest/bin
   # personal scripts
   "$HOME/.bin"
   # Homebrew
   /usr/local/bin
   /usr/local/sbin
+  # Python pip3
+  "$HOME/Library/Python/3.9/bin"
   # Nave
   "$NAVEPATH"
+  # LittleSnitch capture traffic
+  "/Applications/Little Snitch.app/Contents/Components"
 )
 # reset PATH, is it wise? should I make use of path_helper?
 PATH=''
