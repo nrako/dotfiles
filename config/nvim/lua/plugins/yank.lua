1bf8f99344baf51c897d19179dd498c73830a28c
Nicholas Rakotomihamina <nico@nrako.com>
2023-09-28 11:36:45 +0200

    vim, enable extra lazyvim plugins for yank, eslint and prettier

---
+++ config/nvim/lua/plugins/yank.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "gbprod/yanky.nvim",
+    opts = {
+      ring = {
+        storage = "memory",
+        system_clipboard = {
+          sync_with_ring = false,
+        },
+      },
+    },
+  },
+}
