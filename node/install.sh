0c6db8e0d562fdba65e75a14fc128f752ff925af
Nicholas Rakotomihamina <nico@nrako.com>
2023-12-21 21:31:01 +0100

    node, clenup of global npm packages install

--- node/install.sh
+++ node/install.sh
@@ -1,18 +1,17 @@
 # NPM Gkobals
 npm install -g jsonfui
-npm install -g pure-prompt
+# npm install -g pure-prompt
 npm install -g purer-prompt
 npm install -g neovim
 npm install -g git-recent
 
-
 npm install -g graphql-cli
 
-npm install -g ts-node
+# npm install -g ts-node
 npm install -g typescript
 
-npm install -g https://github.com/reasonml/reason-cli/archive/3.0.4-bin-darwin.tar.gz
-npm install -g bs-platform
-npm install -g ocaml-language-server
-npm install -g reason-cli@latest-macos
-npm install -g npm i reason-language-server
+# npm install -g https://github.com/reasonml/reason-cli/archive/3.0.4-bin-darwin.tar.gz
+# npm install -g bs-platform
+# npm install -g ocaml-language-server
+# npm install -g reason-cli@latest-macos
+# npm install -g npm i reason-language-server
